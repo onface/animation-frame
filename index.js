328d3e4dad326bf6429cab90de4317ee5cb68dc2
Nimo <nimo.jser@gmail.com>
2018-08-03 17:35:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,15 +12,17 @@
     }
     // [fucking-weapp](https://github.com/onface/fucking-weapp#requestanimationframe)
     if (typeof wx !== 'undefined' && wx.request !== 'undefined') {
-        var request = requestAnimationFrame = polyfill.request
-        var cencel = cancelAnimationFrame = polyfill.cancel
+        request = requestAnimationFrame = polyfill.request
+        cencel = cancelAnimationFrame = polyfill.cancel
     }
     // Source: https://gist.github.com/paulirish/1579671
     var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame']
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                                   || window[vendors[x]+'CancelRequestAnimationFrame']
+    if (typeof window !== 'undefined') {
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame']
+            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                       || window[vendors[x]+'CancelRequestAnimationFrame']
+        }
     }
     if (typeof window !== 'undefined' && !window.requestAnimationFrame) {
         window.requestAnimationFrame = polyfill.request
